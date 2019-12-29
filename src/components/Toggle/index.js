2e2c9a588ef772b094b7f731dab078de84ba2d13
nomastickles <tnickles@gmail.com>
2019-12-29 10:14:38 -0800

    simple html changes

--- src/components/Toggle/index.js
+++ src/components/Toggle/index.js
@@ -4,8 +4,8 @@ function Toggle({ onChange, expandAll }) {
   return (
     <div className='is-pulled-right'>
       <div className='field is-pulled-right'>
-        <input onChange={onChange} id='switchRtlExample' type='checkbox' name='switchRtlExample' className='switch' checked={expandAll} />
-        <label htmlFor='switchRtlExample'>Expand All</label>
+        <input onChange={onChange} id='toggle' type='checkbox' name='toggle' className='switch' checked={expandAll} />
+        <label htmlFor='toggle'>Expand All</label>
       </div>
     </div>
   )
