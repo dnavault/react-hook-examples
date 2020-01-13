f282286f276654ab1c5d77e0cb0acb4010cad547
nomastickles <tnickles@gmail.com>
2020-01-12 19:47:33 -0800

    useEffect example

---
+++ examples/useeffect-simple/src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Section from "./Section";
+
+import "./styles.scss";
+
+const apiKey = process.env.API_KEY;
+const searchTerm = "art";
+const fetchUrl = `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&limit=50&api_key=9XVJXMWpaIfCbPWQYsrSiTWl5jUWi0op`;
+
+function App() {
+  const [apiData, setApiData] = React.useState([]);
+  React.useEffect(() => {
+    (async () => {
+      const response = await fetch(fetchUrl);
+      const incomingData = await response.json();
+      setApiData(incomingData.data);
+    })();
+  }, []);
+
+  return (
+    <div className="main">
+      <Section apiData={apiData} />
+      <Section apiData={apiData} />
+      <Section apiData={apiData} />
+      <Section apiData={apiData} />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
